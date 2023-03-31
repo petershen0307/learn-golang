bc387c07adacdde8a157cdbb3ef10837b01fb069
Peter Shen <peter_shen@trendmicro.com>
2023-03-31 14:55:33 -0400

    learn go rod for web scraper

---
+++ learn-go-rod/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/go-rod/rod"
+
+func main() {
+    page := rod.New().MustConnect().MustPage("https://www.wikipedia.org/")
+    page.MustWaitLoad().MustScreenshot("a.png")
+}
