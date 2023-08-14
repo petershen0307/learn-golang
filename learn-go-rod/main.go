b55787caf90f93f8c42bc60d1d7df867166ea025
Peter Shen <peter_shen@trendmicro.com>
2023-08-14 12:00:26 -0400

    update go-rod

--- learn-go-rod/main.go
+++ learn-go-rod/main.go
@@ -3,6 +3,8 @@ package main
 import "github.com/go-rod/rod"
 
 func main() {
-    page := rod.New().MustConnect().MustPage("https://www.wikipedia.org/")
-    page.MustWaitLoad().MustScreenshot("a.png")
+	page := rod.New().MustConnect().MustPage("https://www.youtube.com/playlist?list=PLVpaYHJMoSnqKTDtbGYUzgpyEdXctTYK3")
+	page.MustElement("#page-manager > ytd-browse > ytd-playlist-header-renderer > div > div.immersive-header-content.style-scope.ytd-playlist-header-renderer > div.thumbnail-and-metadata-wrapper.style-scope.ytd-playlist-header-renderer > div > div.play-menu.spaced-row.wide-screen-form.style-scope.ytd-playlist-header-renderer > ytd-button-renderer.play-button.style-scope.ytd-playlist-header-renderer > yt-button-shape > a > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill").MustClick()
+	c := make(chan struct{})
+	<-c
 }
