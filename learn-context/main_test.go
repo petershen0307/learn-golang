85f717f4b0a8cf20e556089f6ad2eb279649e46e
Peter Shen <petershen0307@hotmail.com>
2024-01-20 18:06:40 -0500

    add some context example

--- learn-context/main_test.go
+++ learn-context/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -9,19 +10,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func query(ctx context.Context, id int, result chan int) {
+func query(ctx context.Context, id int, result chan string) {
 	timer := time.NewTimer(2 * time.Second)
 	select {
 	case <-ctx.Done():
-		log.Println("id=", id, "query cancel!")
+		s := fmt.Sprint("id=", id, "query cancel!")
+		log.Println(s)
+		result <- s
 	case <-timer.C:
-		log.Println("id=", id, "query finish!")
-		result <- id
+		s := fmt.Sprint("id=", id, "query finish!")
+		log.Println(s)
+		result <- s
 	}
 }
 
-func queryTask(ctx context.Context, queryCount int) chan int {
-	resultChan := make(chan int, queryCount)
+func queryTask(ctx context.Context, queryCount int) chan string {
+	resultChan := make(chan string, queryCount)
 	for i := 0; i != queryCount; i++ {
 		go query(ctx, i, resultChan)
 	}
@@ -36,9 +40,10 @@ func Test_query(t *testing.T) {
 
 	time.Sleep(3 * time.Second)
 	assert.Equal(t, queryCount, len(resultChan))
-	log.Println("result length", len(resultChan))
 	for i := 0; i != queryCount; i++ {
-		log.Println("result", <-resultChan)
+		s := <-resultChan
+		assert.Contains(t, s, "query finish!")
+		log.Println("result", s)
 	}
 }
 
@@ -51,6 +56,106 @@ func Test_cancel_query(t *testing.T) {
 	time.Sleep(1 * time.Second)
 	cancelFunc()
 	time.Sleep(1 * time.Second)
-	assert.Equal(t, 0, len(resultChan))
-	log.Println("result length", len(resultChan))
+	assert.Equal(t, queryCount, len(resultChan))
+	for i := 0; i != queryCount; i++ {
+		s := <-resultChan
+		assert.Contains(t, s, "query cancel!")
+		log.Println("result", s)
+	}
+}
+
+func Test_spawn_context_one_layer_cancel_from_leaf(t *testing.T) {
+	ctx1, _ := context.WithCancel(context.Background())
+	ctx11, cancelFunc11 := context.WithCancel(ctx1)
+	ctx12, cancelFunc12 := context.WithCancel(ctx1)
+	resultChan := make(chan string, 2)
+
+	go query(ctx11, 1, resultChan)
+	go query(ctx12, 2, resultChan)
+	cancelFunc11()
+	cancelFunc12()
+
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, 2, len(resultChan))
+	for i := 0; i != 2; i++ {
+		s := <-resultChan
+		assert.Contains(t, s, "query cancel!")
+		log.Println("result", s)
+	}
+}
+
+func Test_spawn_context_one_layer_cancel_from_root(t *testing.T) {
+	ctx1, cancelFunc1 := context.WithCancel(context.Background())
+	ctx11, _ := context.WithCancel(ctx1)
+	ctx12, _ := context.WithCancel(ctx1)
+	resultChan := make(chan string, 2)
+
+	go query(ctx11, 1, resultChan)
+	go query(ctx12, 2, resultChan)
+	cancelFunc1()
+
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, 2, len(resultChan))
+	for i := 0; i != 2; i++ {
+		s := <-resultChan
+		assert.Contains(t, s, "query cancel!")
+		log.Println("result", s)
+	}
+}
+
+func Test_spawn_context_multiple_layer_cancel_from_leaf(t *testing.T) {
+	ctx1, _ := context.WithCancel(context.Background())
+	ctx2, _ := context.WithCancel(ctx1)
+	ctx3, cancelFunc3 := context.WithCancel(ctx2)
+	resultChan := make(chan string, 2)
+
+	go query(ctx2, 1, resultChan)
+	go query(ctx3, 2, resultChan)
+	cancelFunc3()
+
+	time.Sleep(1 * time.Second)
+	s := <-resultChan
+	assert.Contains(t, s, "query cancel!")
+	log.Println("result", s)
+	s = <-resultChan
+	assert.Contains(t, s, "query finish!")
+	log.Println("result", s)
+}
+
+func Test_spawn_context_multiple_layer_cancel_from_middle(t *testing.T) {
+	ctx1, _ := context.WithCancel(context.Background())
+	ctx2, cancelFunc2 := context.WithCancel(ctx1)
+	ctx3, _ := context.WithCancel(ctx2)
+	resultChan := make(chan string, 2)
+
+	go query(ctx2, 1, resultChan)
+	go query(ctx3, 2, resultChan)
+	cancelFunc2()
+
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, 2, len(resultChan))
+	for i := 0; i != 2; i++ {
+		s := <-resultChan
+		assert.Contains(t, s, "query cancel!")
+		log.Println("result", s)
+	}
+}
+
+func Test_spawn_context_multiple_layer_cancel_from_root(t *testing.T) {
+	ctx1, cancelFunc1 := context.WithCancel(context.Background())
+	ctx2, _ := context.WithCancel(ctx1)
+	ctx3, _ := context.WithCancel(ctx2)
+	resultChan := make(chan string, 2)
+
+	go query(ctx2, 1, resultChan)
+	go query(ctx3, 2, resultChan)
+	cancelFunc1()
+
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, 2, len(resultChan))
+	for i := 0; i != 2; i++ {
+		s := <-resultChan
+		assert.Contains(t, s, "query cancel!")
+		log.Println("result", s)
+	}
 }
