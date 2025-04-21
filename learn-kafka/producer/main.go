7d81b634828f65331b2b5f628897ecfb52f0efcd
Peter Shen <petershen0307@hotmail.com>
2025-04-21 10:16:18 +0800

    use confluent kafka library
    try to fix consumer issue

--- learn-kafka/producer/main.go
+++ learn-kafka/producer/main.go
@@ -7,32 +7,51 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 
 	root "github.com/petershen0307/learn-golang/learn-kafka"
 
-	"github.com/segmentio/kafka-go"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func main() {
 	// Kafka broker address
 	const brokerAddress = "localhost:9092"
 	// Kafka topic
-	const topic = "test-topic"
+	topic := "test-topic"
 
 	// Create a Kafka writer
-	writer := &kafka.Writer{
-		Addr:     kafka.TCP(brokerAddress),
-		Topic:    topic,
-		Balancer: &kafka.RoundRobin{},
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": brokerAddress,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create producer: %s", err)
 	}
-	defer writer.Close()
+
 	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for e := range producer.Events() {
+			switch m := e.(type) {
+			case *kafka.Message:
+				if m.TopicPartition.Error != nil {
+					log.Fatalf("Delivery failed: %v", m.TopicPartition.Error)
+				} else {
+					fmt.Printf("Delivered message to %v\n", m.TopicPartition)
+				}
+			}
+		}
+		wg.Done()
+	}()
 	i := 0
 	for {
 		select {
 		case <-ctx.Done():
 			fmt.Println("Context done, exiting...")
+			producer.Close()
+			wg.Wait()
 			return
 		default:
 			message := root.Message{
@@ -45,12 +64,13 @@ func main() {
 				log.Fatalf("Error marshalling message: %v", err)
 			}
 
-			if err := writer.WriteMessages(ctx,
-				kafka.Message{
-					Value: mdata,
-				}); err != nil {
-				log.Fatalf("Error writing message: %v", err)
+			if err := producer.Produce(&kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+				Value:          mdata,
+			}, nil); err != nil {
+				log.Fatalf("Failed to produce message: %s", err)
 			}
+
 			fmt.Printf("Sent message: %#v\n", message)
 			i++
 			time.Sleep(time.Second)
